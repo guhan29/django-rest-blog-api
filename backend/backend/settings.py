e4399a6d50652c9ddb030fff554e969edad9d45a
Guhan M <mguhan439@gmail.com>
2021-10-18 22:47:55 +0530

    completed authentication

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,19 +37,35 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites', # new
 
     # 3rd party
     'rest_framework',
+    'rest_framework.authtoken',
+    # all auth
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
+    'rest_auth',
+    'rest_auth.registration',
 
     # Local
     'posts.apps.PostsConfig',
 ]
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SITE_ID = 1
+
 # new
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_LASSES': [
+    'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
 
 MIDDLEWARE = [
